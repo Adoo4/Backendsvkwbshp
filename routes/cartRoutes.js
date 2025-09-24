da200f14f6ab8f199a0efd1e6ffcd763b29c2bd8
User Name <email>
2025-09-25 00:37:23 +0200

    Update 1.7

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Cart = require("../models/Cart");
+const Cart = require("../models/cart");
 const Book = require("../models/book");
 const requireAuth = require("../middleware/requireAuth");
 

ad4c39b646130b7f0a438bc83df3d617c0ec366e
User Name <email>
2025-09-25 08:20:47 +0200

    User model Update

--- models/user.js
+++ models/user.js
@@ -2,18 +2,12 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    clerkId: { 
-      type: String, 
-      required: true, 
-      unique: true 
-    }, // Clerk user ID
-
+    clerkId: { type: String, required: true, unique: true },
     email: { type: String },
     name: { type: String },
     role: { type: String, enum: ["user", "admin"], default: "user" },
 
-    cart: { type: mongoose.Schema.Types.ObjectId, ref: "Cart" },
-    wishlist: { type: mongoose.Schema.Types.ObjectId, ref: "Wishlist" },
+    wishlist: { type: mongoose.Schema.Types.ObjectId, ref: "Wishlist" }, // ovo može ostati
   },
   { timestamps: true }
 );

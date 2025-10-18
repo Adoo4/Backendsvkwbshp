667fbc43b6090913439b1abe51d250c2463cdd8a
User Name <email>
2025-10-19 01:12:09 +0200

    callback route

--- routes/payment.js
+++ routes/payment.js
@@ -40,6 +40,11 @@ router.post("/payment-complete", (req, res) => {
   res.send("Payment processed successfully");
 });
 
+router.post("/callback", (req, res) => {
+  console.log("Callback body:", req.body);
+  res.status(200).send("OK");
+});
+
 module.exports = router;
 
 

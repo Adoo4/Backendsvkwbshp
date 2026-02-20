55df5dc03d089a1f55ce979e610b4f4df39ffc7b
User Name <email>
2026-02-20 23:51:00 +0100

    auth

--- middleware/requireAuth.js
+++ middleware/requireAuth.js
@@ -1,4 +1,4 @@
-const { clerk } = require("../index");
+{/*const { clerk } = require("../index");
 const User = require("../models/user");
 
 module.exports = async function requireAuth(req, res, next) {
@@ -73,6 +73,44 @@ module.exports = async function requireAuth(req, res, next) {
     res.status(401).json({ message: "Unauthorized" });
   }
 };
+    console.error("Auth middleware error:", err);
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};*/}
+
+
+const { clerk } = require("../index");
+
+module.exports = async function requireAuth(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ message: "No token provided" });
+
+    const token = authHeader.replace("Bearer ", "");
+
+    // Use JWT verify with the correct template
+    const { claims } = await clerk.jwt.verify(token, { template: "backend" });
+
+    if (!claims || !claims.sub) return res.status(401).json({ message: "Invalid token" });
+
+    const clerkUser = await clerk.users.getUser(claims.sub);
+
+    const email =
+      clerkUser.emailAddresses.find((e) => e.primary)?.emailAddress ||
+      clerkUser.emailAddresses[0]?.emailAddress ||
+      "";
+
+    const user = await User.findOneAndUpdate(
+      { clerkId: clerkUser.id },
+      { clerkId: clerkUser.id, email, name: clerkUser.firstName || "NoName" },
+      { new: true, upsert: true }
+    );
+
+    req.userId = user._id;
+    req.user = user;
+
+    next();
+  } catch (err) {
     console.error("Auth middleware error:", err);
     res.status(401).json({ message: "Unauthorized" });
   }
